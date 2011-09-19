e2f39b47cdcde3e5b2e0a8c16ff1c24beb7b28cc
Akkarit Sangpetch <asangpet@andrew.cmu.edu>
2011-09-18 22:44:57 -0400

    Test thymeleaf template

--- src/main/java/edu/cmu/tactic/config/WebConfig.java
+++ src/main/java/edu/cmu/tactic/config/WebConfig.java
@@ -6,16 +6,38 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateMode;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+	/*
 	@Bean 
 	public InternalResourceViewResolver configureInternalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+	*/
+
+	@Bean
+	public ThymeleafViewResolver configureThymeleafViewResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML5);
+
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+		
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setTemplateEngine(engine);
+		
+		return thymeleafViewResolver;
 	}
 	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
