d4314e7340e8b1c50ce688d5600de391a281f859
Akkarit Sangpetch <asangpet@andrew.cmu.edu>
2011-10-01 22:28:00 -0400

    Cleanup pet example

--- src/main/java/edu/cmu/tactic/controllers/HomeController.java
+++ src/main/java/edu/cmu/tactic/controllers/HomeController.java
@@ -1,26 +1,18 @@
 package edu.cmu.tactic.controllers;
 
 import java.text.DateFormat;
-
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-
 import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import edu.cmu.tactic.data.Response;
-import edu.cmu.tactic.model.Pet;
 import edu.cmu.tactic.services.ResponseDataService;
 
 /**
@@ -57,12 +49,4 @@ public class HomeController {
 		return response;
 	}
 	
-	@RequestMapping(value = "/pets", consumes="application/json", method = RequestMethod.POST)
-	public @ResponseBody List<Pet> showPet(@RequestBody Pet pet, Model model) {
-		logger.info(pet.toString());
-		List<Pet> l = new ArrayList<Pet>();
-		l.add(pet);
-		l.add(pet);
-		return l;
-	}
 }
--- src/main/java/edu/cmu/tactic/model/Pet.java
+++
@@ -1,6 +0,0 @@
-package edu.cmu.tactic.model;
-
-public class Pet {
-	public String name;
-	public int price;
-}
